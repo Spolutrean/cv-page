ade13ad52698ab8472cdc8bd1b8992835b1ab1d6
Sviridov Yaroslav <ys9617890962@gmail.com>
2020-09-08 14:23:56 +0300

    Initial code commit

---
+++ src/main/java/spolutrean/CvPage/CvPageApplication.java
@@ -0,0 +1,13 @@
+package spolutrean.CvPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spolutrean/CvPage/CvPageApplicationTests.java
@@ -0,0 +1,13 @@
+package spolutrean.CvPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
